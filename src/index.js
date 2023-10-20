f7a931e21b73a66cc2e1b18ea1347758f775b169
MikeYan01 <yly374779040@gmail.com>
2023-10-20 11:36:46 +0800

    Update dependencies packages, rosters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import request from "request";
+import axios from "axios";
 import cheerio from "cheerio";
 import fs from "fs";
 
@@ -23,36 +23,31 @@ async function getPlayersUrlsFromEachTeam(team) {
     let teamUrl = getTeamsUrl(team);
 
     const options = {
-        url: teamUrl,
         headers: {
             'User-Agent': 'request'
         }
     }
 
-    let promise = new Promise((resolve, reject) => {
-        request(options, (error, response, body) => {
-            if (error === null && response.statusCode === 200) {
-                let tbody = cheerio.load(body)('tbody');
-                let table = tbody[0];
-                let entries = cheerio.load(table)('.entry-font');
-                
-                for (let entry of entries) {
-                    let playerUrl = cheerio.load(entry)('a').attr("href");
-                    playerUrls.push(playerUrl);
-                }
-    
-                if (playerUrls.length > 0) {
-                    resolve(playerUrls);
-                } else {
-                    reject("Empty playerUrls length");
-                }
-            } else {
-                reject("Failed to get player detail response.");
-            }
-        });
-    });
+    try {
+        const response = await axios.get(teamUrl, options);
+        let tbody = cheerio.load(response.data)('tbody');
+        let table = tbody[0];
+        let entries = cheerio.load(table)('.entry-font');
+        
+        for (let entry of entries) {
+            let playerUrl = cheerio.load(entry)('a').attr("href");
+            playerUrls.push(playerUrl);
+        }
 
-    return promise;
+        if (playerUrls.length > 0) {
+            return playerUrls;
+        } else {
+            throw new Error("Empty playerUrls length");
+        }
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to get player detail response.");
+    }
 }
 
 /**
@@ -60,122 +55,116 @@ async function getPlayersUrlsFromEachTeam(team) {
  */
 async function getPlayerDetail(team, playerUrl) {
     const options = {
-        url: playerUrl,
         headers: {
             'User-Agent': 'request'
         }
     }
 
-    let promise = new Promise((resolve, reject) => {
-        request(options, (error, response, body) => {
-            if (error === null && response.statusCode === 200) {
-                var p = new player();
+    try {
+        const response = await axios.get(playerUrl, options);
+        var p = new player();
 
-                // name
-                let nameDiv = cheerio.load(body)('h1');
-                p.name = nameDiv.text().trim();
+        // name
+        let nameDiv = cheerio.load(response.data)('h1');
+        p.name = nameDiv.text().trim();
 
-                // overall attribute
-                let overallAttribute = cheerio.load(body)('.attribute-box-player');
-                p.overallAttribute = parseInt(overallAttribute.text().trim());
+        // overall attribute
+        let overallAttribute = cheerio.load(response.data)('.attribute-box-player');
+        p.overallAttribute = parseInt(overallAttribute.text().trim());
 
-                // team
-                p.team = team;
+        // team
+        p.team = team;
 
-                let attributes = cheerio.load(body)('.content .card .card-body .list-no-bullet li span');
-                
-                // outside scoring
-                let closeShot = attributes[0].children[0].data.trim();
-                p.closeShot = parseInt(closeShot);
-                let midRangeShot = attributes[1].children[0].data.trim();
-                p.midRangeShot = parseInt(midRangeShot);
-                let threePointShot = attributes[2].children[0].data.trim();
-                p.threePointShot = parseInt(threePointShot);
-                let freeThrow = attributes[3].children[0].data.trim();
-                p.freeThrow = parseInt(freeThrow);
-                let shotIQ = attributes[4].children[0].data.trim();
-                p.shotIQ = parseInt(shotIQ);
-                let offensiveConsistency = attributes[5].children[0].data.trim();
-                p.offensiveConsistency = parseInt(offensiveConsistency);
+        let attributes = cheerio.load(response.data)('.content .card .card-body .list-no-bullet li span');
 
-                // athleticism
-                let speed = attributes[6].children[0].data.trim();
-                p.speed = parseInt(speed);
-                let acceleration = attributes[7].children[0].data.trim();
-                p.acceleration = parseInt(acceleration);
-                let strength = attributes[8].children[0].data.trim();
-                p.strength = parseInt(strength);
-                let vertical = attributes[9].children[0].data.trim();
-                p.vertical = parseInt(vertical);
-                let stamina = attributes[10].children[0].data.trim();
-                p.stamina = parseInt(stamina);
-                let hustle = attributes[11].children[0].data.trim();
-                p.hustle = parseInt(hustle);
-                let overallDurability = attributes[12].children[0].data.trim();
-                p.overallDurability = parseInt(overallDurability);
+        // outside scoring
+        let closeShot = attributes[0].children[0].data.trim();
+        p.closeShot = parseInt(closeShot);
+        let midRangeShot = attributes[1].children[0].data.trim();
+        p.midRangeShot = parseInt(midRangeShot);
+        let threePointShot = attributes[2].children[0].data.trim();
+        p.threePointShot = parseInt(threePointShot);
+        let freeThrow = attributes[3].children[0].data.trim();
+        p.freeThrow = parseInt(freeThrow);
+        let shotIQ = attributes[4].children[0].data.trim();
+        p.shotIQ = parseInt(shotIQ);
+        let offensiveConsistency = attributes[5].children[0].data.trim();
+        p.offensiveConsistency = parseInt(offensiveConsistency);
 
-                // inside scoring
-                let layup = attributes[13].children[0].data.trim();
-                p.layup = parseInt(layup);
-                let standingDunk = attributes[14].children[0].data.trim();
-                p.standingDunk = parseInt(standingDunk);
-                let drivingDunk = attributes[15].children[0].data.trim();
-                p.drivingDunk = parseInt(drivingDunk);
-                let postHook = attributes[16].children[0].data.trim();
-                p.postHook = parseInt(postHook);
-                let postFade = attributes[17].children[0].data.trim();
-                p.postFade = parseInt(postFade);
-                let postControl = attributes[18].children[0].data.trim();
-                p.postControl = parseInt(postControl);
-                let drawFoul = attributes[19].children[0].data.trim();
-                p.drawFoul = parseInt(drawFoul);
-                let hands = attributes[20].children[0].data.trim();
-                p.hands = parseInt(hands);
+        // athleticism
+        let speed = attributes[6].children[0].data.trim();
+        p.speed = parseInt(speed);
+        let acceleration = attributes[7].children[0].data.trim();
+        p.acceleration = parseInt(acceleration);
+        let strength = attributes[8].children[0].data.trim();
+        p.strength = parseInt(strength);
+        let vertical = attributes[9].children[0].data.trim();
+        p.vertical = parseInt(vertical);
+        let stamina = attributes[10].children[0].data.trim();
+        p.stamina = parseInt(stamina);
+        let hustle = attributes[11].children[0].data.trim();
+        p.hustle = parseInt(hustle);
+        let overallDurability = attributes[12].children[0].data.trim();
+        p.overallDurability = parseInt(overallDurability);
 
-                // playmaking
-                let passAccuracy = attributes[21].children[0].data.trim();
-                p.passAccuracy = parseInt(passAccuracy);
-                let ballHandle = attributes[22].children[0].data.trim();
-                p.ballHandle = parseInt(ballHandle);
-                let speedWithBall = attributes[23].children[0].data.trim();
-                p.speedWithBall = parseInt(speedWithBall);
-                let passIQ = attributes[24].children[0].data.trim();
-                p.passIQ = parseInt(passIQ);
-                let passVision = attributes[25].children[0].data.trim();
-                p.passVision = parseInt(passVision);
+        // inside scoring
+        let layup = attributes[13].children[0].data.trim();
+        p.layup = parseInt(layup);
+        let standingDunk = attributes[14].children[0].data.trim();
+        p.standingDunk = parseInt(standingDunk);
+        let drivingDunk = attributes[15].children[0].data.trim();
+        p.drivingDunk = parseInt(drivingDunk);
+        let postHook = attributes[16].children[0].data.trim();
+        p.postHook = parseInt(postHook);
+        let postFade = attributes[17].children[0].data.trim();
+        p.postFade = parseInt(postFade);
+        let postControl = attributes[18].children[0].data.trim();
+        p.postControl = parseInt(postControl);
+        let drawFoul = attributes[19].children[0].data.trim();
+        p.drawFoul = parseInt(drawFoul);
+        let hands = attributes[20].children[0].data.trim();
+        p.hands = parseInt(hands);
 
-                // defending
-                let interiorDefense = attributes[26].children[0].data.trim();
-                p.interiorDefense = parseInt(interiorDefense);
-                let perimeterDefense = attributes[27].children[0].data.trim();
-                p.perimeterDefense = parseInt(perimeterDefense);
-                let steal = attributes[28].children[0].data.trim();
-                p.steal = parseInt(steal);
-                let block = attributes[29].children[0].data.trim();
-                p.block = parseInt(block);
-                let lateralQuickness = attributes[30].children[0].data.trim();
-                p.lateralQuickness = parseInt(lateralQuickness);
-                let helpDefenseIQ = attributes[31].children[0].data.trim();
-                p.helpDefenseIQ = parseInt(helpDefenseIQ);
-                let passPerception = attributes[32].children[0].data.trim();
-                p.passPerception = parseInt(passPerception);
-                let defensiveConsistency = attributes[33].children[0].data.trim();
-                p.defensiveConsistency = parseInt(defensiveConsistency);
+        // playmaking
+        let passAccuracy = attributes[21].children[0].data.trim();
+        p.passAccuracy = parseInt(passAccuracy);
+        let ballHandle = attributes[22].children[0].data.trim();
+        p.ballHandle = parseInt(ballHandle);
+        let speedWithBall = attributes[23].children[0].data.trim();
+        p.speedWithBall = parseInt(speedWithBall);
+        let passIQ = attributes[24].children[0].data.trim();
+        p.passIQ = parseInt(passIQ);
+        let passVision = attributes[25].children[0].data.trim();
+        p.passVision = parseInt(passVision);
 
-                // rebounding
-                let offensiveRebound = attributes[34].children[0].data.trim();
-                p.offensiveRebound = parseInt(offensiveRebound);
-                let defensiveRebound = attributes[35].children[0].data.trim();
-                p.defensiveRebound = parseInt(defensiveRebound);
-    
-                resolve(p);
-            } else {
-                reject("Failed to get player details");
-            }
-        });
-    });
+        // defending
+        let interiorDefense = attributes[26].children[0].data.trim();
+        p.interiorDefense = parseInt(interiorDefense);
+        let perimeterDefense = attributes[27].children[0].data.trim();
+        p.perimeterDefense = parseInt(perimeterDefense);
+        let steal = attributes[28].children[0].data.trim();
+        p.steal = parseInt(steal);
+        let block = attributes[29].children[0].data.trim();
+        p.block = parseInt(block);
+        let lateralQuickness = attributes[30].children[0].data.trim();
+        p.lateralQuickness = parseInt(lateralQuickness);
+        let helpDefenseIQ = attributes[31].children[0].data.trim();
+        p.helpDefenseIQ = parseInt(helpDefenseIQ);
+        let passPerception = attributes[32].children[0].data.trim();
+        p.passPerception = parseInt(passPerception);
+        let defensiveConsistency = attributes[33].children[0].data.trim();
+        p.defensiveConsistency = parseInt(defensiveConsistency);
 
-    return promise;
+        // rebounding
+        let offensiveRebound = attributes[34].children[0].data.trim();
+        p.offensiveRebound = parseInt(offensiveRebound);
+        let defensiveRebound = attributes[35].children[0].data.trim();
+        p.defensiveRebound = parseInt(defensiveRebound);
+
+        return p;
+    } catch (error) {
+        throw new Error("Failed to get player details");
+    }
 }
 
 /**
@@ -194,8 +183,8 @@ function sortPlayers(a, b) {
  * Sava data to local disk. Every new run generates a new file.
  */
 function saveData(db) {
-    var filePath = './data/roster.json';
-    var data = JSON.stringify(db, null, 4);
+    let filePath = './data/roster.json';
+    let data = JSON.stringify(db, null, 4);
     
     fs.writeFile(filePath, data, error => {
         if (error == null) {
@@ -206,7 +195,7 @@ function saveData(db) {
     })
 }
 
-var __main = async function() {
+const main = async function() {
     let teams = CURRENT_TEAMS;
 
     // <teams, all player urls>
@@ -215,13 +204,13 @@ var __main = async function() {
     // all players details
     var players = [];
 
-    console.log("################ Start to get player urls ... ################");
+    console.log("################ Fetching player urls ... ################");
     await Promise.all(teams.map(async team => {
         let playerUrls = await getPlayersUrlsFromEachTeam(team);
         roster.set(team, playerUrls);
     }));
 
-    console.log("################ Start to get player details ... ################");
+    console.log("################ Fetching player details ... ################");
     for (let team of teams) {
         let playerUrls = roster.get(team);
         let prettiedTeamName = teamNamePrettier(team);
@@ -231,14 +220,14 @@ var __main = async function() {
         await Promise.all(playerUrls.map(async playerUrl => {
             let player = await getPlayerDetail(prettiedTeamName, playerUrl);
             players.push(player);
-            console.log(`Successfully get ${player.name}'s detail.`);
+            console.log(`Successfully fetched ${player.name}'s detail.`);
         }));
     }
 
     players.sort(sortPlayers);
 
-    console.log("################ Save to disk ... ################");
+    console.log("################ Saving to disk ... ################");
     saveData(players);
 }
 
-__main()
+main()
